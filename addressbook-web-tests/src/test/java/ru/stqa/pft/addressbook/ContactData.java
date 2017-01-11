62969c71896846e230256375df707afcbfdf2637
ekatanaev <evgenykatanaev@gmail.com>
2017-01-11 11:49:45 +0300

    Создание контакта и рефакторинг

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String lastName;
+  private final String company;
+  private final String tellNumber;
+  private final String email;
+
+  public ContactData(String firstName, String lastName, String company, String tellNumber, String email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.company = company;
+    this.tellNumber = tellNumber;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getTellNumber() {
+    return tellNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
