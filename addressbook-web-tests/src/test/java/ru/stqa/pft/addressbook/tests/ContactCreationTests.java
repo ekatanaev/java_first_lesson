cc9d116e491318922381acb960adb3c1cc359438
ekatanaev <evgenykatanaev@gmail.com>
2017-01-15 13:33:42 +0300

    Оптимизация заполнения форм: оставляем не изменяющися значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     app.getContactHelper().gotoContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("Evgen", "Kat", "Ino", "123456789", "Ino@mail.ru"));
+    app.getContactHelper().fillContactForm(new ContactData("Evgen", "Kat", "Ino", null, null));
     app.getContactHelper().submitContactCreation();
     app.getContactHelper().returnToContactPage();
   }
