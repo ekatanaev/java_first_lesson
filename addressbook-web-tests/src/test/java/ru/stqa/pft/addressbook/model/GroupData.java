97db202b877c0a80f2168d8ddde1d0e96e146876
ekatanaev <evgenykatanaev@gmail.com>
2017-02-06 16:53:17 +0300

    Object Relational Mapping (ORM) Гиперссылка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,13 +16,18 @@ import javax.persistence.Table;
 public class GroupData {
   @XStreamOmitField
   @Id
-  @Column(name = "group_list")
+  @Column(name = "group_id")
   private int id = Integer.MAX_VALUE;
+
   @Column(name = "group_name")
   private String name;
+
   @Column(name = "group_header")
+  @Type(type = "text")
   private String header;
+
   @Column(name = "group_footer")
+  @Type(type = "text")
   private String footer;
 
   public int getId() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -26,7 +26,8 @@ public class HbConnectionTest {
             .build();
     try {
       sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-    } catch (Exception e) {
+    }
+    catch (Exception e) {
       e.printStackTrace();
       // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
       // so destroy it manually.
